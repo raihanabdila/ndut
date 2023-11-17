ebd0967ad4686f81d84a8dce8eecd39ff9d537d1
raihanabdila <raihamafdila770@gmail.com>
2023-11-17 18:10:39 +0700

    PUSH sarah kakakku

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        h4 {
+            padding: 15px ;
+            font-style: oblique
+
+        }
+        p {
+            text-align: justify ;
+            padding : 15px ;
+        }
+
+
+    </style>
+</head>
+<body>
+    <h1>Ini Adalah Halaman Profile</h1>
+    <ul>
+        <li><a href="/">home</a></li>
+        <li><a href="/about">about</a></li>
+        <li><a href="/mahasiswa">mahasiswa</a></li>
+        <li><a href="/profile">profile</a></li>
+    </ul>
+    <div class="row">
+        <div class="col-sm-6">
+            <h4>What is Lorem Ipsum?</h4>
+            <p> <b>Lorem ipsum</b> is simply dummy text of the printing and 
+                typesetting industry.Lorem ipsum has been the industry's standard
+                dummy text ever since the 1500s.When an unknwon printer took a
+                galley of type and scrambled it to make a type specimen book.
+                it has survived not only five centuries, but also the leap into 
+                electronic typesetting,remaining essentially unchanged. it was 
+                popularised in the 1960s with the release of letrast sheets containing Lorem 
+                ipsum passages, and more recently with desktop publishing software 
+                like aldus pagemaker including version of Lorem Ipsum.
+            </p>
+        </div>
+        <div class="col-sm-6">
+            <h4>Why do we use it?</h4>
+            <p>it is a long established fact that a reader will be distracted by the
+                readable content of a page when looking at its layout. The point ofusing
+                Lorem Ipsum is that it has a more or-less normal distibution of letters, 
+                as opposed to using 'Content here, content here'. 
+                making it look like readable English. Many desktop publishing packages and
+                web page editor now ise Lorem Ipsum as their default model text, and a search
+                for 'lorem ipsum' will uncover many web sites still in their infacy. 
+                Various versions have evolved over the years, sometimes by accident, sometimes
+                on purpose (injected humour and the like).
+            </p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <h4>Where does it come from?</h4>
+            <p>
+                Contrary to popular belief. Lorem Ipsum is not simply random text.
+                it has roots in a piece of classical Latin literature from 45 BC.
+                making it over 2000 years old. Ricard McClintock, a Latin professor
+                at Hampden_Sydney Collage in Virginia.Looked up one of the more 
+                obscure Latin word, consectur, from a Lorem Ipsum passage, and going
+                through the cites of the word in classical literacture, discovered the
+                undoubtable sorce.Lorem Ipsum comes from section 1.10.32 and 1.10.33 
+                of "de Finibus Bonorum er Malorum" (The Extreme of Good and Evil) 
+                by cicero, written in 45 BC.This book is a treatise on the theory
+                of ethisc, very popular during the Renaissance. The first lie of Lorem Ipsum, 
+                "Lorem Ipsum dolor sit amt..",comes froma line in section 1.10.32.
+            </p>
+        </div>
+        <div class="col-sm-6">
+            <h4>Where can i get some?</h4>
+            <p>
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered alteration in some form, by injected
+                humour, or randomised words which don't look even slightly
+                believable. If you are going to use a passage of Lorem Ipsum, 
+                you need to be sure there isn't anything embrassing hidden in the
+                middle of text. All the Lorem Ipsum generators on the internet
+                tend to repeaat predefinef chunks as necessary, making this the Latin words,
+                combined with a handful of model sentence structure, to generate 
+                Lorem Ipsum which looks reasnable. The generated Lorem Ipsum is therefore
+                always free from repetition.injected humour, or non-characteristic words etc.
+            </p>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('internet dasar/tugas1');
+});
+Route::get('/about', function () {
+    return view('pertemuan4/home');
+});
+Route::get('/', function () {
+    return view('pertemuan4/home');
+});
+Route::get('/', function () {
+    return view('pertemuan4/home');
+});
+Route::get('/', function () {
+    return view('pertemuan4/home');
+});
+Route::get('/', function () {
+    return view('pertemuan4/home');
+});
